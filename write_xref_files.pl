57d481d394e060139dda2e965f86a9abd06f1d37
Daniel Lawson <lawson.vectorbase@gmail.com>
2015-01-27 13:00:41 +0000

    various tweaks, full verbose output added

--- write_xref_files.pl
+++ write_xref_files.pl
@@ -9,7 +9,8 @@ my $verboser;
 my $help;
 my $summary;
 my $full;
-my $organism;         # Code (4 letters) to designate which species to process
+my $all,
+my $organism;        # Code (4 letters) to designate which species to process
 my @organism;        
 my $filetype;        # Xref file type to generate 
 # filetype options
@@ -19,7 +20,9 @@ my $citation;        # write citation file
 my $description;     # write description file
 # other
 my $release;         # Release version
+my $updated;         # updated?
 my $protcod;         # protein-coding only
+my $rna;             # ncRNA only
 my $write;           # write output to file 
 my $terminal;        # write output to terminal
 # Synonyms
@@ -27,7 +30,9 @@ my %synonym_data;
 # Summary
 my %summary_data;
 my %sum_citation;
-
+my %sum_families;
+my %update_author;
+my %update_species;
 # get organism and spreadsheet data
 my %organism2code;
 my %code2organism;
@@ -42,9 +47,11 @@ GetOptions (
     # Species
     "organism=s{2}" => \@organism,
     "release=s"     => \$release,
+    "updated=s"     => \$updated,
     # run mode
     "summary"       => \$summary,
     "full"          => \$full,
+    "all"           => \$all,
     # Xref file
     #"filetype"      => \$filetype,
     "symbol"        => \$symbol,
@@ -52,6 +59,7 @@ GetOptions (
     "citation"      => \$citation,
     "description"   => \$description,
     "protcod"       => \$protcod,
+    "rna"           => \$rna,
     # Output
     "write"         => \$write,
     "terminal"      => \$terminal,
@@ -64,13 +72,13 @@ if ( $help ) {
 }
 
 # Check that we have a release version
-unless ( $release ) {
+unless ( $release || $updated) {
   print "# $0 " . gmtime( time()) ."\n\n";
   print "No release declared. Aborting.\n";
   print " '-release' option to set the VectorBase release version\n\n";
   exit(0);
 }
-unless ( ($release) =~ (/^VB\-\d+\-\d+$/) ) {
+unless ( ($release) =~ (/^VB\-\d+\-\d+$/) || $updated) {
   print "# $0 " . gmtime( time()) ."\n\n";
   print "Release string looks weird. It should be of the format VB-2001-01.\n";
   print " '-release' is set to '$release'\n";
@@ -81,7 +89,7 @@ unless ( ($release) =~ (/^VB\-\d+\-\d+$/) ) {
 $organism = join ' ',@organism;
 
 # Check that we have declared a species
-unless ( $organism || $summary ) {
+unless ( $organism || $summary || $all) {
   print "# $0 " . gmtime( time()) ."\n\n";
   print "No organism declared. Aborting.\n";
   print " '-organism' option to set organism to process\n\n";
@@ -89,7 +97,7 @@ unless ( $organism || $summary ) {
 }
 
 # Check that we have declared a filetype
-unless ( $symbol || $synonym || $citation || $description || $full || $summary ) {
+unless ( $symbol || $synonym || $citation || $description || $full || $summary || $updated ) {
   print "# $0 " . gmtime( time()) ."\n\n";
   print "No filetype declared. Aborting.\n";
   print "Choose one of the following:\n '-symbol', '-synonym', '-citation', '-description'\n\n";
@@ -97,7 +105,7 @@ unless ( $symbol || $synonym || $citation || $description || $full || $summary )
 }
 # Assign filetype
 $filetype = "symbol"      if ( $symbol );
-$filetype = "synonym"     if ( $synonym );
+$filetype = "synonym"     if ( $synonym  );
 $filetype = "citation"    if ( $citation );
 $filetype = "description" if ( $description );
 
@@ -108,66 +116,83 @@ $filetype = "description" if ( $description );
 ##
 
 # '-summary' mode for summary of all species
-if ( $summary ) {
+if ( $summary && !$updated) {
 	# open filehandle for output file
 	open (OUTPUT, "> ${release}_xref_data.txt") if ( $write );
 	# print table header
 	print "+---------------------+                           +--------------------------------+--------------------------------+\n";
 	print "| Release: ".$release." |                           | Protein-coding CDS             | non-coding RNA                 |\n";
-	print "+---------------------+-----+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+\n";
-	print "| Organism                  | Rows     | Active   | CDS      | Symbol   | Descript | ncRNA    | Symbol   | Descript | Synonym  | Citation | Papers   |\n";
-	print "+---------------------------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+\n";
+	print "+---------------------+-----+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+\n";
+	print "| Organism                  | Rows     | Active   | CDS      | Symbol   | Describe | ncRNA    | Symbol   | Describe | Synonym  | Families | Uniq Fam | Citation | Papers   |\n";
+	print "+---------------------------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+\n";
+
 	# print organism data rows
-	while (<DATA>) {
-    	chomp;
-    	last if /^__END__$/;         # stop when __END__ is encountered
-    	next if /^\s*(#.*|\s*)$/;    # skip blank lines or comment lines
-		&parse_spreadsheet($_);
-		%summary_data = "";
-		%sum_citation = "";
+	if  ( $organism ) {
+		&parse_spreadsheet($organism);
+	}
+	else {
+		while (<DATA>) {
+    		chomp;
+    		last if /^__END__$/;         # stop when __END__ is encountered
+    		next if /^\s*(#.*|\s*)$/;    # skip blank lines or comment lines
+			%summary_data = "";
+			&parse_spreadsheet($_);
+			%sum_citation = "";
+			%sum_families = "";
+		}
 	}
 	# print table footer
-	print "+---------------------------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+\n";
+	print "+---------------------------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+\n";
 	# close filehandle for output file
 	close OUTPUT if ( $write );
 }
 # '-full' mode exports all data types for a single organism
-elsif ( $full ) {
+elsif ( $full || $all) {
 #	my @runs = ('symbol','synoym','citation','description');
 
-	$symbol = 1;
-	my $filename = "${release}_".join('-',@organism)."_xref_symbol.txt";
-	print "// Exporting symbol for $organism\n";
-	print "// Writing output to $filename\n";
-	open (OUTPUT, "> $filename");
-	&parse_spreadsheet($organism);
-	close OUTPUT;
-	undef $symbol;
-	$synonym = 1;
-	my $filename = "${release}_".join('-',@organism)."_xref_synonym.txt";
-	print "// Exporting synonym for $organism\n";
-	print "// Writing output to $filename\n";
-	open (OUTPUT, "> $filename");
-	&parse_spreadsheet($organism);
-	close OUTPUT;
-	undef $synonym;
-	$citation = 1;
-	my $filename = "${release}_".join('-',@organism)."_xref_citation.txt";
-	print "// Exporting citation for $organism\n";
-	print "// Writing output to $filename\n";
-	open (OUTPUT, "> $filename");
-	&parse_spreadsheet($organism);
-	close OUTPUT;
-	undef $citation;
-	$description = 1;
-	my $filename = "${release}_".join('-',@organism)."_xref_description.txt";
-	print "// Exporting description for $organism\n";
-	print "// Writing output to $filename\n";
-	open (OUTPUT, "> $filename");
-	&parse_spreadsheet($organism);
-	close OUTPUT;
-	undef $description;
 
+	# print organism data rows
+	if  ( $organism ) {
+		&export_spreadsheet($organism);
+	}
+	else {
+		while (<DATA>) {
+    		chomp;
+    		last if /^__END__$/;         # stop when __END__ is encountered
+    		next if /^\s*(#.*|\s*)$/;    # skip blank lines or comment lines
+
+			undef %synonym_data;         # clear any legacy synonym data
+
+			&export_spreadsheet($_);
+		}
+	}
+}
+# -updated mode, display rows modified since timestamp $updated
+elsif ( $updated ) {
+
+	# print organism data rows
+	if  ( $organism ) {
+		&parse_spreadsheet($organism);
+	}
+	else {
+		while (<DATA>) {
+    		chomp;
+    		last if /^__END__$/;         # stop when __END__ is encountered
+    		next if /^\s*(#.*|\s*)$/;    # skip blank lines or comment lines
+#    		%summary_data = "";
+			&parse_spreadsheet($_);
+		}
+	}
+	print "\n";
+	print "// Some overview stats for gene metadata additions since $updated\n//\n";
+	print "// No. of updated symbols:   $summary_data{Symbol}\n";
+	print "// No. of updated synonyms:  $summary_data{Synonym}\n";
+	print "// No. of updated citations: $summary_data{Citation}\n";
+	print "// Species:\n";
+	foreach my $i (sort {$update_species{$b} <=> $update_species{$a} or "$a" cmp "$b"} keys %update_species) {printf ("// => %3s\t$i\n", $update_species{$i})}
+	print "// Submitters:\n";
+	foreach my $i (sort {$update_author{$b} <=> $update_author{$a} or "$a" cmp "$b"}   keys %update_author)  {printf ("// => %3s\t$i\n", $update_author{$i})}
+	print "\n";
 }
 # default exports a single data type for a single organism
 else {
@@ -197,13 +222,19 @@ sub parse_spreadsheet {
 	# Get gene metadata via curl and process
 	#---------------------------------------------------------#
 
-	open (FILE, "curl --silent $curlcommand |");
+	open (FILE, "curl --silent $curlcommand |") or print "// WARNING failed to open spreadsheet\n";
 	while (<FILE>) {
 
-		# write row to output file if -summary -write option for debugging purposes
-		if ( ( $summary ) && ( $write ) ) { print OUTPUT;}
+		# ignore header lines
+		next if (/VB_ID/);
 
 		chomp;
+		# write row to output file if -summary -write option for debugging purposes
+		if ( ( $summary ) && ( $write ) ) { 
+			print OUTPUT "$_\n";
+		}
+
+
 		@f = split/\t/;
 
 		# Symbol file {get data and write file}
@@ -215,6 +246,9 @@ sub parse_spreadsheet {
  			next unless ( $f[7] ne "");
  			# Protein-coding only?
  			if ( $protcod ) { next unless $f[3] eq "protein_coding";}
+ 			# ncRNA only?
+ 			if ( $rna ) { next unless $f[3] =~ /RNA/ ;}
+
 			# write data to OUTPUT filehandle or STDOUT
  			if ( $terminal ) {
 				print "$f[0]\t$f[1]\tgene\t$f[7]\t$f[10]\n";
@@ -272,28 +306,57 @@ sub parse_spreadsheet {
 
 		# Summary file {get data}
 		#---------------------------------------------------------#
-		if ( $summary) {
+		if ( $summary && !$updated ) {
 			$summary_data{Entries}++;
 			$summary_data{Active}++         if ( $f[2] eq "ACTIVE" );
 
 			$summary_data{CDS}++            if ( $f[3] eq "protein_coding");
 			$summary_data{ncRNA}++          if ( $f[3] =~ /RNA/);
 
-			$summary_data{Symbol}++         if ( $f[7] ne "" );
+			$summary_data{Symbol}++         if ( $f[7]  ne "" );
 			$summary_data{Symbol_CDS}++     if ( ($f[7] ne "") && ($f[3] eq "protein_coding") );
 			$summary_data{Symbol_ncRNA}++   if ( ($f[7] ne "") && ($f[3] =~ /RNA/) );
 
 			$summary_data{Synonym}++        if ( $f[2] eq "SYNONYM" );
 
-			$summary_data{Description}++    if ( $f[10] ne "" );
+			$summary_data{Description}++    if ( $f[10]  ne "" );
 			$summary_data{Desc_CDS}++       if ( ($f[10] ne "") && ($f[3] eq "protein_coding") );
 			$summary_data{Desc_ncRNA}++     if ( ($f[10] ne "") && ($f[3] =~ /RNA/) );
 
+			$summary_data{Family}++         if ( $f[9] ne "" );
+
+			if ( $f[9] ne "" ) {
+				$sum_families{$f[9]}++;
+			}
 
 			if ( $f[2] eq "CITATION" ) {
 				$summary_data{Citation}++;
 				$sum_citation{$f[12]}++;
  			}	
+		}
+		# Updated summary
+		#---------------------------------------------------------#
+		if ( $updated ) {
+			# $f[15]
+			my $date = $f[15];
+			$date =~ s/-//g;
+			if ( $date > $updated) {
+				printf ("[$date >> $updated] %16s %-25s %-25s [AC: $f[1] GN: %-10s DE: %-64s]\n", $f[0], $f[11], $f[13], $f[7], substr($f[10],0,64) ) if ( $verbose );
+
+				# Warnings for incomplete curations..
+				# Submitter
+				if ( $f[11] eq "" ) { printf ("WARNING: $f[0] %-25s has no submitter name\n", $f[13]); }
+				# Species
+				if ( $f[13] eq "" ) { printf ("WARNING: $f[0] (Author: %-25s) has no species name\n", $f[11]); }
+
+				# Count update type
+				if ( $f[2] eq "ACTIVE")      {$summary_data{Symbol}++;}
+				elsif ( $f[2] eq "SYNONYM")  {$summary_data{Synonym}++;}
+				elsif ( $f[2] eq "CITATION") {$summary_data{Citation}++;}
+				# Push update species & author
+				$update_species{$f[13]}++;
+				$update_author{$f[11]}++;
+			}
 		}
 	}
 	close FILE;
@@ -323,8 +386,10 @@ sub parse_spreadsheet {
 
 	# Summary file {write file}
 	#---------------------------------------------------------#
-	if ( $summary ) {
-		my $no_papers = scalar (keys %sum_citation) - 1;
+	if ( $summary && !$updated) {
+		my $no_papers   = scalar (keys %sum_citation) - 1;  # 2015-01-14 added -1 modifier
+		my $no_families = scalar (keys %sum_families) - 1;  # 2015-01-14 added -1 modifier
+
 		# if verbose write long list, else table row
 		if ( $verbose ) {
 			print "Organism:                  $organism\n";
@@ -341,18 +406,70 @@ sub parse_spreadsheet {
 			print "No. of Description ncRNA:  $summary_data{Desc_ncRNA}\n";
 			# synonyms
 			print "No. of Synonyms:           $summary_data{Synonym}\n";
+			# families
+			print "No. of Family members:     $summary_data{Family}\n";
+			print "No. of Families:           $no_families\n";
+
+			foreach my $i (sort keys %sum_families){print "=> $sum_families{$i}\t$i\n";}
+
 			# citations
 			print "No. of Citations:          $summary_data{Citation}\n";
 			print "No. of Papers:             $no_papers\n";
 			print "\n";
 		}
 		else {
-			printf ("| %-25s | %8s | %8s | %8s | %8s | %8s | %8s | %8s | %8s | %8s | %8s | %8s |\n", $organism,$summary_data{Entries},$summary_data{Active},$summary_data{CDS},$summary_data{Symbol_CDS},$summary_data{Desc_CDS},$summary_data{ncRNA},$summary_data{Symbol_ncRNA},$summary_data{Desc_ncRNA},$summary_data{Synonym},$summary_data{Citation},$no_papers);
+			printf ("| %-25s | %8s | %8s | %8s | %8s | %8s | %8s | %8s | %8s | %8s | %8s | %8s | %8s | %8s |\n", $organism,$summary_data{Entries},$summary_data{Active},$summary_data{CDS},$summary_data{Symbol_CDS},$summary_data{Desc_CDS},$summary_data{ncRNA},$summary_data{Symbol_ncRNA},$summary_data{Desc_ncRNA},$summary_data{Synonym},$summary_data{Family},$no_families,$summary_data{Citation},$no_papers);
 		}
 	}
 	#_ end of summary reporting
 }
 #_ end of parse_spreadsheet subroutine
+
+sub export_spreadsheet {
+
+	my ($organism) = @_;
+	my $organism4filename = $organism;
+	$organism4filename =~ s/ /_/;
+
+#	print "// Exporting \n";
+
+	if ( $symbol || $full ) {
+		my $filename = "${release}_${organism4filename}_xref_symbol.txt";
+		print "// Exporting symbol for $organism ($organism4filename)\n";
+		print "// Writing output to $filename\n";
+		open (OUTPUT, "> $filename");
+		&parse_spreadsheet($organism);
+		close OUTPUT;
+	}
+
+	if ( $synonym || $full ) {
+		my $filename = "${release}_${organism4filename}_xref_synonym.txt";
+		print "// Exporting synonym for $organism ($organism4filename)\n";
+		print "// Writing output to $filename\n";
+		open (OUTPUT, "> $filename");
+		&parse_spreadsheet($organism);
+		close OUTPUT;
+	}
+
+	if ( $citation || $full ) {
+		my $filename = "${release}_${organism4filename}_xref_citation.txt";
+		print "// Exporting citation for $organism ($organism4filename)\n";
+		print "// Writing output to $filename\n";
+		open (OUTPUT, "> $filename");
+		&parse_spreadsheet($organism);
+		close OUTPUT;
+	}
+
+	if ( $description || $full ) {
+		my $filename = "${release}_${organism4filename}_xref_description.txt";
+		print "// Exporting description for $organism ($organism4filename)\n";
+		print "// Writing output to $filename\n";
+		open (OUTPUT, "> $filename");
+		&parse_spreadsheet($organism);
+		close OUTPUT;
+	}
+}
+#_ end of export_spreadsheet subroutine
 
 ## Fields from spreadsheet 
 # Curation_ID  
@@ -383,6 +500,7 @@ sub getdata {
 		'Anopheles arabiensis'      => 'AARA',
 		'Anopheles atroparvus'      => 'AATE',
 		'Anopheles christyi'        => 'ACHR',
+		'Anopheles coluzzii'        => 'ACOM',
 		'Anopheles culicifacies'    => 'ACUA',
 		'Anopheles darlingi'        => 'ADAC',
 		'Anopheles dirus'           => 'ADIR',
@@ -399,7 +517,7 @@ sub getdata {
 		'Anopheles stephensi'       => 'ASTE',
 		'Biomphalaria glabrata'     => 'BGLB',
 		'Culex quinquefasciatus'    => 'CPIJ',
-		'Glossina austeni'          => 'GAUI',
+		'Glossina austeni'          => 'GAUT',
 		'Glossina brevipalpis'      => 'GBRI',
 		'Glossina fuscipes'         => 'GFUI',
 		'Glossina morsitans'        => 'GMOY',
@@ -421,6 +539,7 @@ sub getdata {
 		'AARA' => '1a5OtCuDUGxbrg_0rMcbDQHUHXtwbzO_g9vTm_K896UY',
 		'AATE' => '1oxUeyJVjzrq4oe5LlVqHzEGB1Hd2_7_75sCGuWxEkT0',
 		'ACHR' => '1QqhBJB4vlArR9jwaQvYSbxVGnljPRuBWQvUqLmibr7U',
+		'ACOM' => '1Qc7GVQO4OXIPBuiIzITDPzjbKffsnobBg7s2NmeCr7Q',
 		'ACUA' => '1Y9FjfL5bYHHSgDx26sRLLCMNVIlqqyT6T_ny9n0XKwQ',
 		'ADAC' => '1dcOTVBAAtfV1Vksekr1BOZ0NKxUl8zqWiz12ULmQ2hQ',
 		'ADIR' => '1haqU3Z0pctX3G1sMhVb8lk0GDtEvGObmODEcFwqMJwE',
@@ -436,7 +555,11 @@ sub getdata {
 		'ASIN' => '1NQL-9mmykZ0fWls6405mhCKSxIVfE2kvxaFRogb1tXY',
 		'ASTE' => '1ZVMIQ_tr3zJBMuk-KdUcEXSIqVOjL8jANc_SKqQAlkU',
 		'CPIJ' => '1_6KHioJdjHYroDooP48FavcclKs1D2vqxPZvJzcTq_U',
+		'GAUT' => '1VWQ8g_JXkz8L4XLvJJ5AVGwjhr4NP5am25-DUtJcjc0',
+		'GBRI' => '1n8LzP0VOEUbgsFtkr2rX6FJNUEfr9CGCfKikrI0WdVo',
+		'GFUI' => '131u1wnBlJaIlbLbmih2zu2q1lCZhPFK3oNfo9LD5gY0',
 		'GMOY' => '1OytW4SzLunXCphzrX_mz6CwIsfn-zIzrt8eYmy6nbBk',
+		'GPAI' => '1qh1tGOHr21wKHSq6r6JwNNLE-x1K4x7kDxSEXULpaus',
 		'ISCW' => '10hoIT-Zhsk1byeB5nissrbPeKXGKO5ODt0rPwTzk8fY',
 		'MDOA' => '1ZNXGtXI_6L_qJjz8iMt02vNCFVdlD4Z0iisSqM2hw1A',
 		'PHUM' => '1WeBgVThFg8mW8-xrqtfMSWmSEGk9iwKi-8RWZDTucEg',
@@ -454,6 +577,7 @@ Anopheles albimanus
 Anopheles arabiensis
 Anopheles atroparvus
 Anopheles christyi
+Anopheles coluzzii
 Anopheles culicifacies
 Anopheles darlingi
 Anopheles dirus
@@ -470,11 +594,11 @@ Anopheles sinensis
 Anopheles stephensi
 #Biomphalaria glabrata
 Culex quinquefasciatus
-#Glossina austeni
-#Glossina brevipalpis
-#Glossina fuscipes
+Glossina austeni
+Glossina brevipalpis
+Glossina fuscipes
 Glossina morsitans
-#Glossina pallidipes
+Glossina pallidipes
 Ixodes scapularis
 #Lutzomyia longipalpis
 Musca domestica
@@ -490,9 +614,49 @@ __END__
 
 =head2 USAGE 
 
-This script writes tsv files for use in VectorBase xref pipelines
+This script writes tsv files for use in VectorBase xref pipelines.
 
-=head2 ARGUMENTSy
+=head3 Examples..
+
+=over 2
+
+=item -find updates for all species since a defined timepoint, 
+
+Use the I<-updated> option to find annotations that have been added or modifed since a particular date. If you add the I<-verbose> flag to get a list of the changes.
+
+=over 2
+
+=item All species, 
+
+C<-updated 20150101 -summary>
+
+=item Single species, 
+
+C<-updated 20150101 -organism Anopheles gambiae>
+
+=back
+
+=item -write data to flatfile (for external use)
+
+=over 2
+
+=item All species, 
+
+C<-release VB-2015-02 -symbol -write>, export single datatype for all species
+
+C<-release VB-2015-02 -full -write -summary>, export all data types for all species
+
+=item Single species, 
+
+C<-release VB-2015-02 -organism Anopheles gambiae -symbol -write>, export single datatype for single species
+
+C<-release  VB-2015-02 -organism Anopheles gambiae -full -write>, export single datatype for single species
+
+=back
+
+=back 
+
+=head2 ARGUMENTS
 
 B<write_xref_files.pl> arguments:
 
